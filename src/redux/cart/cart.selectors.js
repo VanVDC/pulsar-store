e0c731a2b53be8711a483e2a633365c5963222c7
VanVDC <vanbvy@gmail.com>
2020-02-04 22:09:39 -0500

    change the cart selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,10 +9,14 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+//
+
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
 );
+
+//
 
 export const selectCartTotal = createSelector([selectCartItems], cartItems =>
   cartItems.reduce(
