d3930a0ed7dc2e8a48400eec4f88ae9f6886e3f4
VanVDC <vanbvy@gmail.com>
2020-02-08 21:01:46 -0500

    add fake credit card warning

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,6 +35,10 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments* 4242 4242 4242
+      4242 -exp: 01/21 -cw 123
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 );
