963bfea54f8af7b3f9d674d6316de934a85eef3f
VanVDC <vanbvy@gmail.com>
2020-02-16 20:53:45 -0500

    install redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
 import rootReducers from "./root-reducers";
 
 import logger from "redux-logger";
 
-const middleware = [];
+const middleware = [thunk];
 
 if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
