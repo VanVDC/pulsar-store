e314db1b4826b23c8086cc8f3e48853128d640a0
VanVDC <vanbvy@gmail.com>
2020-02-20 20:08:46 -0500

    added root-saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,10 +1,5 @@
 import ShopActionTypes from "./shop.types";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
-
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
@@ -18,19 +13,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-// export const fetchCollectionsStartAsync = () => {
-//   return dispatch => {
-//     const collectionRef = firestore.collection("collection");
-
-//     dispatch(fetchCollectionsStart());
-
-//     collectionRef
-//       .get()
-//       .then(snapshot => {
-//         const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-//         dispatch(fetchCollectionsSuccess(collectionMap));
-//       })
-//       .catch(error => dispatch(fetchCollectionsFailure(error.message)));
-//   };
-// };
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "redux-saga";
 
-import { fetchCollectionsStart } from "../redux/shop/shop.sagas";
-
 import rootReducers from "./root-reducers";
+import rootSaga from "./root-saga";
 
 import logger from "redux-logger";
 
@@ -18,6 +17,6 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducers, applyMiddleware(...middleware));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
