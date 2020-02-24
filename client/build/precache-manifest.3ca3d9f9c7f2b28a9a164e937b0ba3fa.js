81c20992500c12bd08d8eeee177d7ea51829cf1d
VanVDC <vanbvy@gmail.com>
2020-02-23 23:40:27 -0500

    adding npm install --only=dev && npm install && npm run build

---
+++ client/build/precache-manifest.3ca3d9f9c7f2b28a9a164e937b0ba3fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5a490a3dca7ca65ab8b1fed30e1c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f76f5abc2c1c02b26c",
+    "url": "/static/css/main.e09a63dd.chunk.css"
+  },
+  {
+    "revision": "c47629ba10fd88da9771",
+    "url": "/static/js/2.69d1ac20.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.69d1ac20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5f76f5abc2c1c02b26c",
+    "url": "/static/js/main.5f4a2728.chunk.js"
+  },
+  {
+    "revision": "d52f329488f261f1e8cf",
+    "url": "/static/js/runtime-main.e301ff1b.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.svg.30f07071.svg"
+  }
+]);
