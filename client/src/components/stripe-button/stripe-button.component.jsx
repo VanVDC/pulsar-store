015c6507a4619088525ed91595f5411d7b271b0c
VanVDC <vanbvy@gmail.com>
2020-02-23 22:52:19 -0500

    created express server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import StripeCheckout from "react-stripe-checkout";
 
+//the hell
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_rX6VriD3nh3Z29VULG4TWrDm00vAfoN5W0";
