303bd0d0b1de1c542e2bd58b456a7a4c0833828e
VanVDC <vanbvy@gmail.com>
2020-02-23 22:34:15 -0500

    seperated client and created a express server to handle payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_rX6VriD3nh3Z29VULG4TWrDm00vAfoN5W0";
+
+  const onToken = token => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment is successful");
+      })
+      .catch(error => {
+        console.log("Paymen error: ", JSON.parse(error));
+        alert("There was an issue with your payment.");
+      });
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Pulsar Store"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
