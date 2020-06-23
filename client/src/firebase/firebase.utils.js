af4ddacd649a124bcb4aff91c527af39eba2dfb7
VanVDC <vanbvy@gmail.com>
2020-06-23 13:22:10 -0400

    added another env

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -13,6 +13,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_GOOGLE_MEASUREMENT_ID,
 };
 
+console.log("firebasecon", process.env.REACT_APP_GOOGLE_API_KEY);
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
