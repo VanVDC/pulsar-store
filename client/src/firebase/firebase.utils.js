3a310c7609beeba2d7d33a4872c91324decdc6a1
VanVDC <vanbvy@gmail.com>
2020-07-04 21:07:04 -0400

    change the firebase keys

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,22 +3,22 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  // authDomain: process.env.REACT_APP_GOOGLE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_GOOGLE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_GOOGLE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_GOOGLE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_GOOGLE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_GOOGLE_APP_ID,
-  // measurementId: process.env.REACT_APP_GOOGLE_MEASUREMENT_ID,
-  apiKey: "AIzaSyACYiTseI4sGGmOo4hmENrcPGTtUOPj1qM",
-  authDomain: "pulsar-fb2aa.firebaseapp.com",
-  databaseURL: "https://pulsar-fb2aa.firebaseio.com",
-  projectId: "pulsar-fb2aa",
-  storageBucket: "pulsar-fb2aa.appspot.com",
-  messagingSenderId: "366397027551",
-  appId: "1:366397027551:web:db0ddcd5116e44f510a56a",
-  measurementId: "G-X5K9M0Z0JY",
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  authDomain: process.env.REACT_APP_GOOGLE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_GOOGLE_DATABASE_URL,
+  projectId: process.env.REACT_APP_GOOGLE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_GOOGLE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_GOOGLE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_GOOGLE_APP_ID,
+  measurementId: process.env.REACT_APP_GOOGLE_MEASUREMENT_ID,
+  // apiKey: "AIzaSyACYiTseI4sGGmOo4hmENrcPGTtUOPj1qM",
+  // authDomain: "pulsar-fb2aa.firebaseapp.com",
+  // databaseURL: "https://pulsar-fb2aa.firebaseio.com",
+  // projectId: "pulsar-fb2aa",
+  // storageBucket: "pulsar-fb2aa.appspot.com",
+  // messagingSenderId: "366397027551",
+  // appId: "1:366397027551:web:db0ddcd5116e44f510a56a",
+  // measurementId: "G-X5K9M0Z0JY",
 };
 
 // Initialize Firebase
