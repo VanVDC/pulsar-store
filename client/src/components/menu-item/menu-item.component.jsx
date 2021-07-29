240846cff67aa8cc748eea7cebb91505ab4405a1
VanVDC <vanbvy@gmail.com>
2021-07-29 15:36:45 -0400

    fixed pointer error

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import "./menu-item.styles.scss";
+import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => (
   <div className={`${size} menu-item`}>
     <div
-      className="background-image"
+      className='background-image'
       style={{ backgroundImage: `url(${imageUrl})` }}
-      onClick={() => history.push(`${match.url}${linkUrl}`)}
+      // onClick={() => history.push(`${match.url}${linkUrl}`)}
     />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">Shop Now</span>
+    <div
+      className='content'
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>Shop Now</span>
     </div>
   </div>
 );
