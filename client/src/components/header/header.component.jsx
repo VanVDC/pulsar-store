dbffb73aed7d74cfae1fce5adf090af58010016c
VanVDC <vanbvy@gmail.com>
2021-07-30 13:41:38 -0400

    fix header

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -25,9 +25,9 @@ const Header = ({ currentUser, hidden, signOutStart }) => (
       <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      {/* <Link className='option' to='/shop'>
         CONTACT
-      </Link>
+      </Link> */}
       {currentUser ? (
         <div className='option' onClick={signOutStart}>
           SIGN OUT
